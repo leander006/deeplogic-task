8fab77644f21cc6f82946ec0ff5c6ea24b625c94
leander006 <leanderdsilva06@gmail.com>
2024-02-28 07:30:29 +0530

    solving cors error

--- frontend/src/services/helper.js
+++ frontend/src/services/helper.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://deeplogic-task.onrender.com";
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://deeplogic-task.onrender.com";
+// export const BASE_URL = "http://localhost:3001";
