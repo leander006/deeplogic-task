0d6836a1cd50b52743eac1cf9e006abb66586c32
leander006 <leanderdsilva06@gmail.com>
2024-02-28 07:20:20 +0530

    optimizing api calls

--- frontend/src/component/Card.jsx
+++ frontend/src/component/Card.jsx
@@ -2,78 +2,50 @@ import React, { useEffect, useState } from 'react'
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { BASE_URL } from '../services/helper';
-import axios from 'axios';
+
 
 Chart.register(CategoryScale);
 
-function Card({title,number,id,get}) {
+function Card({title,number,id,add,dataState}) {
 
-
-      const [dataState, setDataState] = useState([]);
-      const [add, setAdd] = useState(0)
-      const config = {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${localStorage?.getItem("token")}`,
-            },
-          };
-
+  const [chartData, setChartData] = useState({
+    labels: dataState.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: dataState.map((data) => data.userGain),
+        backgroundColor: "#4d8c57", // Area color
+        borderColor: "#013220", // Line color
+        borderWidth: 2,
+        fill: true // Fill area under line
+      }
+    ]
+  });
       useEffect(() => {
-        const fetchData = async () => {
-          try {
-
-            const { data } = await axios.get(
-                  `${BASE_URL}/api/post/data/${get}`,
-                  config
-                );
-                setDataState(data.data);
-                setAdd(data.number) 
-          } catch (error) {
-            console.log(error);
-          }
-        };
-        fetchData();
-      }, [get]);
-    
-      const [chartData, setChartData] = useState({
-        labels: dataState.map((data) => data.year),
-        datasets: [
-          {
-            label: "Users Gained ",
-            data: dataState.map((data) => data.userGain),
-            backgroundColor: "#4d8c57", // Area color
-            borderColor: "#013220", // Line color
-            borderWidth: 2,
-            fill: true // Fill area under line
-          }
-        ]
-      });
-    
-          useEffect(() => {
-            if(id == 1){
-                  setChartData(prevChartData => ({
-                        ...prevChartData,
-                        datasets: prevChartData.datasets.map(dataset => ({
-                          ...dataset,
-                          backgroundColor: "#008E5F" // Replace with your new color
-                        }))
-                      }));
+        setChartData(prevChartData => ({
+          ...prevChartData,
+          labels: dataState.map((data) => data.year),
+          datasets: [
+            {
+              ...prevChartData.datasets[0], // Keep other properties unchanged
+              data: dataState.map((data) => data.userGain)
             }
-          }, [id])
+          ]
+        }));
+      }, [dataState]);
 
-          useEffect(() => {
-            setChartData(prevChartData => ({
-              ...prevChartData,
-              labels: dataState.map((data) => data.year),
-              datasets: [
-                {
-                  ...prevChartData.datasets[0], // Keep other properties unchanged
-                  data: dataState.map((data) => data.userGain)
-                }
-              ]
-            }));
-          }, [dataState]);
+  useEffect(() => {
+    if(id == 1){
+          setChartData(prevChartData => ({
+                ...prevChartData,
+                datasets: prevChartData.datasets.map(dataset => ({
+                  ...dataset,
+                  backgroundColor: "#008E5F" // Replace with your new color
+                }))
+              }));
+    }
+  }, [id])
+
   return (
     <div className={`${id == 1 ?"bg-[#01B075] text-white": "bg-[#202123] text-gray-300" } rounded-md p-3`}>
             <div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import {useLocation} from "react-router-dom"
-import Card from '../component/Card';
-import Chart from "chart.js/auto";
-import { CategoryScale } from "chart.js";
+import Card from '../component/Card';;
+import { BASE_URL } from '../services/helper';
+import axios from 'axios';
 
-Chart.register(CategoryScale);
+
 
 
 function Home({setSelect}) {
@@ -16,6 +16,34 @@ function Home({setSelect}) {
   useEffect(() => {
     setSelect(pathname)
   }, [])
+
+  const [dataState, setDataState] = useState([]);
+  const [add, setAdd] = useState(0)
+  const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage?.getItem("token")}`,
+        },
+      };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+        const { data } = await axios.get(
+              `${BASE_URL}/api/post/data/${get}`,
+              config
+            );
+            setDataState(data.data);
+            setAdd(data.number) 
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [get]);
+
+
 
   return (
     <div className='text-white w-full h-[99vh] overflow-y-scroll'>
@@ -32,10 +60,10 @@ function Home({setSelect}) {
 
 
          <div className='grid  md:grid-cols-2 grid-cols-1 gap-4 mx-3'>
-            <Card id={1} get={get}  title={"Number of post"} number={30}/>
-            <Card id={2} get={get} title={"Post likes"} number={100}/>
-            <Card id={3} get={get} title={"Post shares"} number={20}/>
-            <Card id={4} get={get} title={"Post engagement"} number={40}/>
+            <Card id={1} get={get} add={add} dataState={dataState}  title={"Number of post"} number={30}/>
+            <Card id={2} get={get} add={add} dataState={dataState} title={"Post likes"} number={100}/>
+            <Card id={3} get={get} add={add} dataState={dataState} title={"Post shares"} number={20}/>
+            <Card id={4} get={get} add={add} dataState={dataState} title={"Post engagement"} number={40}/>
          </div>
       </div>      
     </div>
